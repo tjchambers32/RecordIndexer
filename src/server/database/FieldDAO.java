3913eeb4b0cdcb8d0cb792e18ea74ac651099ef3
Travis Chambers <tjchambers32@gmail.com>
2015-02-26 21:05:06 -0700

    added most of Search method

--- src/server/database/FieldDAO.java
+++ src/server/database/FieldDAO.java
@@ -33,14 +33,69 @@ public class FieldDAO {
 		
 	}
 	
-	public Search_Result search(ArrayList<Integer> fields, ArrayList<String> search_values ) {
-		Search_Result result = null;
+	public ArrayList<Search_Result> search(ArrayList<Integer> fields, ArrayList<String> search_values ) throws DatabaseException {
+		
+		ArrayList<Search_Result> result = new ArrayList<Search_Result>();	
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			for (int id : fields) {
+				for (String value : search_values) {
+					String query = "SELECT records.imageID, image.filepath, records.rowNumber, fields.id " +
+							"FROM images, records, fields " +
+							"WHERE images.imageID = records.imageID " +
+							"AND images.projectID = fields.projectID " + 
+							"AND fields.fieldNumber = records.fieldNumber " +
+							"AND fields.ID = ? " +
+							"AND records.value = ? ";
+					
+					stmt = db.getConnection().prepareStatement(query);
+					stmt.setInt(1, id);
+					stmt.setString(2, value);
+					rs = stmt.executeQuery();
+					while(rs.next()) {
+						//TODO: PICK UP HERE!!
+						//return an arraylist of search_results. Each find should be a search_result
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new DatabaseException(e.getMessage(), e);
+		}
 		
 		return result;
 	}
 	
-	public ArrayList<Field> getFields(int projectID) {
-		ArrayList<Field> result = null;
+	public ArrayList<Field> getFields(int projectID) throws DatabaseException {
+		
+		ArrayList<Field> result = new ArrayList<Field>();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			String query = "select * from fields where fields.projectID = ?";
+			stmt = db.getConnection().prepareStatement(query);
+			
+			stmt.setInt(1, projectID);
+			
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				int id = rs.getInt(1);
+				int fieldNumber = rs.getInt(2);
+				String title = rs.getString(3);
+				int xCoord = rs.getInt(4);
+				int width = rs.getInt(5);
+				String helpHTML = rs.getString(6);
+				String knownData = rs.getString(7);
+				int projID = rs.getInt(8);
+
+				result.add(new Field(id, fieldNumber, title, xCoord, width, helpHTML, knownData, projID));
+			}
+			
+		} catch (Exception e) {
+			throw new DatabaseException(e.getMessage(), e);
+		}
 		
 		return result;
 	}
