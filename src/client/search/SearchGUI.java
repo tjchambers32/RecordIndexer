76d0320b271ba2e6f8ecf49eb7e57e8af76cc7da
Travis Chambers <tjchambers32@gmail.com>
2015-03-13 09:56:03 -0600

    playing around with swing

--- src/client/search/SearchGUI.java
+++ src/client/search/SearchGUI.java
@@ -1,8 +1,13 @@
 package client.search;
 
 import java.awt.EventQueue;
+
 import client.communication.ClientCommunicator;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
  * @author tchambs
@@ -19,15 +24,28 @@ public class SearchGUI extends JFrame{
 		super(windowTitle);
 		comm = new ClientCommunicator();
 		
+		initializeComponents();
+		
 	}
 
+	private void initializeComponents() {
+		//TODO: just testing delete when done
+		
+		
+		this.add(new JMenuBar());
+		this.add(new JMenu());
+		this.add(new JMenuItem());
+		
+		
+		// end of testing
+	}
+	
 	public static void main(String[] args) {
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				SearchGUI frame = new SearchGUI("SearchGUI");
-				//frame.setSize(100,100);
-				//frame.pack();
+				frame.pack();
 				frame.setVisible(true);
 			}
 		});
