d6035663f1c99581a987fb010bf92c96cee7739f
Travis Chambers <tjchambers32@gmail.com>
2015-03-14 11:37:54 -0600

    playing around with GUIs some more

--- src/client/search/SearchGUI.java
+++ src/client/search/SearchGUI.java
@@ -1,13 +1,25 @@
 package client.search;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import client.communication.ClientCommunicator;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 /**
  * @author tchambs
@@ -24,20 +36,43 @@ public class SearchGUI extends JFrame{
 		super(windowTitle);
 		comm = new ClientCommunicator();
 		
-		initializeComponents();
+		initialize();
 		
 	}
 
-	private void initializeComponents() {
-		//TODO: just testing delete when done
+	private void initialize() {
+		
+		JLabel hostLabel = new JLabel("Host Name: ");
+		JTextField hostField = new JTextField(20);
+		hostField.setOpaque(true);
+		hostField.setBackground(Color.white);
+		hostField.setPreferredSize(new Dimension(100,20));
+		
+		JDialog dialog = new JDialog(this, "Login", true);
+		
+		dialog.add(hostLabel);	
+		dialog.add(hostField); // hostField covers up hostLabel
+		//whichever comes second just covers up the first component
 		
 		
-		this.add(new JMenuBar());
-		this.add(new JMenu());
-		this.add(new JMenuItem());
+		dialog.setSize(500,500);
+		dialog.setLocation(500,200);
+//		dialog.pack();
+		dialog.setVisible(true);
+		
+//		JMenuBar jmenubar = new JMenuBar();
+//		setJMenuBar(jmenubar);
+//		
+//		JMenu jmenu = new JMenu();
+//		jmenu.setMnemonic('c');
+//		jmenubar.add(jmenu);
+//		
+//		JMenuItem jmenuitem = new JMenuItem("Exit", KeyEvent.VK_X);
+//		jmenu.add(jmenuitem);
+		
+				
 		
 		
-		// end of testing
 	}
 	
 	public static void main(String[] args) {
@@ -45,7 +80,12 @@ public class SearchGUI extends JFrame{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				SearchGUI frame = new SearchGUI("SearchGUI");
-				frame.pack();
+				
+				//sizing for testing
+				frame.setLocation(500,200);
+				frame.setSize(500,500);
+				
+				//frame.pack();
 				frame.setVisible(true);
 			}
 		});
